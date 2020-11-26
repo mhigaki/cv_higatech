135704bbfcc1105da26e2b0ccc158eab4deeda27
Milton N Hgaki <milton.higaki@gmail.com>
2020-11-26 17:33:35 -0300

    ups again

---
+++ js/contact-form-script.js
@@ -0,0 +1,55 @@
+$("#contactForm").validator().on("submit", function(event) {
+  if (event.isDefaultPrevented()) {
+    // handle the invalid form...
+    formError();
+    submitMSG(false, "Você preencheu o formulário corretamente?");
+  } else {
+    // everything looks good!
+    event.preventDefault();
+    submitForm();
+  }
+});
+
+
+function submitForm() {
+  // Initiate Variables With Form Content
+  var name = $("#fname").val();
+  var email = $("#email").val();
+  var msg_subject = $("#msg_subject").val();
+  var message = $("#comment").val();
+
+
+  $.ajax({
+    type: "POST",
+    url: "assets/php/form-process.php",
+    data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&comment=" + message,
+    success: function(text) {
+      if (text == "success") {
+        formSuccess();
+      } else {
+        formError();
+        submitMSG(false, text);
+      }
+    }
+  });
+}
+
+function formSuccess() {
+  $("#contactForm")[0].reset();
+  submitMSG(true, "Mensagem Enviada!")
+}
+
+function formError() {
+  $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+    $(this).removeClass();
+  });
+}
+
+function submitMSG(valid, msg) {
+  if (valid) {
+    var msgClasses = "h3 text-center tada animated text-warning";
+  } else {
+    var msgClasses = "h3 text-center text-danger";
+  }
+  $("#message").removeClass().addClass(msgClasses).text(msg);
+}
